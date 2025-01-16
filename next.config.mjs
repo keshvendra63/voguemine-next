26d671d1558d7f84cb39e828a4484ef171e4f9e3
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-01-16 10:51:33 +0000

    voguemine

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com'], // Add Unsplash domain
-      },
-};
-export default nextConfig;
