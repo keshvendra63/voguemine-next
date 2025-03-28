ac97ceefd9780fb8d254a83d6dc2c63c4a4b5b3e
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-03-28 07:20:26 +0000

    voguemine

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,16 +2,18 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        if (mongoose.connection.readyState === 0) {
-            await mongoose.connect(process.env.MONGO_URL, {
-                // No need for useNewUrlParser and useUnifiedTopology
-            });
-            console.log('Connected to MongoDB successfully');
-        }
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            maxPoolSize: 10, // Limit connections
+          });
+          console.log("MongoDB Connected Successfully!");
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
-        throw err;
+        process.exit(1);
     }
 };
 
 export default connectDb;
+
+
