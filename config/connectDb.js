7081bddac247d9f1d0c9e325cb617d86b488beba
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-04-03 03:21:11 +0000

    voguemine

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 
+let isConnected; // Track the connection status
+
 const connectDb = async () => {
+    if (isConnected) {
+        console.log("MongoDB is already connected.");
+        return; // If already connected, exit the function
+    }
+
     try {
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             maxPoolSize: 10, // Limit connections
-          });
-          console.log("MongoDB Connected Successfully!");
+        });
+        isConnected = true; // Set the connection status to true
+        console.log("MongoDB Connected Successfully!");
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
         process.exit(1);
@@ -15,5 +23,3 @@ const connectDb = async () => {
 };
 
 export default connectDb;
-
-
