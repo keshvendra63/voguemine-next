bb4608b71bad1affb40f1161561da09e873cd2be
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-05-10 07:02:36 +0000

    voguemine

--- src/app/api/collection/trending-collections/route.js
+++ src/app/api/collection/trending-collections/route.js
@@ -11,20 +11,20 @@ export async function GET(request) {
     try {
         await connectDb();
 
-        const mensFeatured = await Men.find({ isTrending: "true",state: "active" });
-        const mensMostFeatured = await Men.find({ mostTrending: "true",state: "active" });
+        const mensFeatured = await Men.find({ isTrending: "true",status: "active" });
+        const mensMostFeatured = await Men.find({ mostTrending: "true",status: "active" });
         const mensFeaturedPrdts = mensMostFeatured.length > 0 ? await Product.find({ collectionHandle: mensMostFeatured[0]?.handle,state: "active" }).sort({ createdAt: -1 }).limit(4) : [];
 
-        const womensFeatured = await Women.find({ isTrending: "true",state: "active" });
-        const womensMostFeatured = await Women.find({ mostTrending: "true",state: "active" });
+        const womensFeatured = await Women.find({ isTrending: "true",status: "active" });
+        const womensMostFeatured = await Women.find({ mostTrending: "true",status: "active" });
         const womensFeaturedPrdts = womensMostFeatured.length > 0 ? await Product.find({ collectionHandle: womensMostFeatured[0]?.handle,state: "active" }).sort({ createdAt: -1 }).limit(4) : [];
 
-        const kidFeatured = await Kid.find({ isTrending: "true",state: "active" });
-        const kidMostFeatured = await Kid.find({ mostTrending: "true",state: "active" });
+        const kidFeatured = await Kid.find({ isTrending: "true",status: "active" });
+        const kidMostFeatured = await Kid.find({ mostTrending: "true",status: "active" });
         const kidFeaturedPrdts = kidMostFeatured.length > 0 ? await Product.find({ collectionHandle: kidMostFeatured[0]?.handle,state: "active" }).sort({ createdAt: -1 }).limit(4) : [];
 
-        const accFeatured = await Accessories.find({ isTrending: "true",state: "active" });
-        const accMostFeatured = await Accessories.find({ mostTrending: "true",state: "active" });
+        const accFeatured = await Accessories.find({ isTrending: "true",status: "active" });
+        const accMostFeatured = await Accessories.find({ mostTrending: "true",status: "active" });
         const accFeaturedPrdts = accMostFeatured.length > 0 ? await Product.find({ collectionHandle: accMostFeatured[0]?.handle,state: "active" }).sort({ createdAt: -1 }).limit(4) : [];
 
         return new Response(
