0af92874e7b3eb17b56ec2f7a26f3f2132ff14d0
keshvendra63 <118011825+keshvendra63@users.noreply.github.com>
2025-05-29 19:45:46 +0000

    voguemine

--- src/app/loading.js
+++
@@ -1,5 +0,0 @@
-export default function Loading() {
-    return <div className='loading'>
-<img src='https://res.cloudinary.com/dqh6bd766/image/upload/c_limit,h_1000,f_auto,q_auto/v1737957198/hqkx6iwzbjhr7oxfyvnq.png' alt='loading-image'/>
-    </div>
-  }
